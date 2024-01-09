86e04c6758f2cf21c244887010c981d20dcdea37
Alex Fazakas <144437816+alexcf04@users.noreply.github.com>
2024-01-09 18:04:16 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,42 +1,52 @@
 import React, { useState } from "react";
 
-//create your first component
+
+// Create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
+
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value);
+	};
+
+	const handleKeyPress = (e) => {
+		if (e.key === "Enter") {
+			setTodos([...todos, inputValue]);
+			setInputValue("");
+		}
+	};
+
+	const handleDelete = (index) => {
+		const updatedTodos = todos.filter((_, i) => i !== index);
+		setTodos(updatedTodos);
+	};
+
 	return (
-		<div className="container">
+		<div className="container mt-5">
 			<h1>My Todos List</h1>
 			<ul>
 				<li>
 					<input
 						type="text"
-						onChange={() => setInputValue(e.target.value)}
-						// value={(inputValue)}
-						onKeyPress={(e) => {
-							console.log(e)
-							if (e.key === "Enter") {
-								setTodos(todos.concat([inputValue]));
-								setInputValue("");
-							}
-						}}
+						value={inputValue}
+						onChange={handleInputChange}
+						onKeyPress={handleKeyPress}
 						placeholder="Que tengo que hacer?"
-					></input>
+					/>
 				</li>
 				{todos.map((item, index) => (
-					<li>
+					<li key={index}>
 						{item}{" "}
-						<i class="fas fa-trash-alt"
-							onClick={() =>
-								setTodos(
-									todos.filter(t, currentIndex) =>
-									index != currentIndex
-						
-							}></i>
+						<i
+							className="fas fa-trash-alt"
+							onClick={() => handleDelete(index)}
+						></i>
 					</li>
 				))}
-		</ul>
-		</div >
+			</ul>
+			<div>{todos.length} tasks</div>
+		</div>
 	);
 };
 
